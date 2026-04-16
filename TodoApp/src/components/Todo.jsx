213edcd7b26ae34badbdb6684f8621082a81c08d
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-04-16 23:50:48 +0530

    todo mark done undone feature added

--- TodoApp/src/components/Todo.jsx
+++ TodoApp/src/components/Todo.jsx
@@ -3,9 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 
 const Todo = () => {
   const [tasksList, setTasksList] = useState([
-    { id: uuidv4(), task: "Eat" },
-    { id: uuidv4(), task: "Code" },
-    { id: uuidv4(), task: "Sleep" },
+    { id: uuidv4(), task: "Eat", done: false },
+    { id: uuidv4(), task: "Code", done: false },
+    { id: uuidv4(), task: "Sleep", done: false },
   ]);
   const [input, setInput] = useState("");
 
@@ -13,7 +13,10 @@ const Todo = () => {
     const trimmed = input.trim();
 
     if (trimmed !== "") {
-      setTasksList((prev) => [...prev, { id: uuidv4(), task: trimmed }]);
+      setTasksList((prev) => [
+        ...prev,
+        { id: uuidv4(), task: trimmed, done: false },
+      ]);
       setInput("");
     } else {
       alert("Please Enter Task First");
@@ -22,6 +25,22 @@ const Todo = () => {
 
   function deleteTask(id) {
     setTasksList((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  function doneUnDone(id) {
+    setTasksList((prev) => {
+      return prev.map((task) => {
+        if (task.id === id) {
+          if (task.done) {
+            return { ...task, done: false };
+          } else {
+            return { ...task, done: true };
+          }
+        } else {
+          return task;
+        }
+      });
+    });
   }
 
   return (
@@ -63,15 +82,31 @@ const Todo = () => {
                   key={task.id}
                   className="flex justify-between items-center bg-gray-700 px-4 py-2 rounded-lg"
                 >
-                  <span className="text-white">
-                    {index + 1}. {task.task}
-                  </span>
+                  {/* LEFT SIDE (grouped) */}
+                  <div className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      checked={task.done}
+                      onChange={() => doneUnDone(task.id)}
+                      className="checkbox checkbox-primary"
+                    />
+                    <span
+                      className={`text-white ${
+                        task.done
+                          ? "line-through opacity-50"
+                          : "hover:text-gray-300"
+                      }`}
+                    >
+                      {task.task}
+                    </span>
+                  </div>
 
+                  {/* RIGHT SIDE */}
                   <button
                     onClick={() => deleteTask(task.id)}
-                    className="bg-red-500 hover:bg-red-400 px-2 py-1 rounded-lg text-white text-sm active:scale-95 transition"
+                    className="rounded-lg text-2xl cursor-pointer text-red-400 font-extrabold active:scale-95 transition"
                   >
-                    🗑️
+                    &times;
                   </button>
                 </li>
               ))}
